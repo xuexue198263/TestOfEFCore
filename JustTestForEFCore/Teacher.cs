89a7273d4ddf5e8e2bd32a5c8e777b2c53294b48
xueduoyu <xueduoyu@DESKTOP-VKIC297>
2020-07-02 22:42:39 +0800

    添加项目文件。

---
+++ JustTestForEFCore/Teacher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustTestForEFCore
+{    
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
